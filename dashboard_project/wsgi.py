d37997ee8a064bc8ab84cbbff319a6064c1eabfa
Patrick Ashi <patrickpeko1@gmail.com>
2024-11-10 00:18:55 +0100

    new

--- dashboard_project/wsgi.py
+++ dashboard_project/wsgi.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard_project.settings')
 
 application = get_wsgi_application()
+
+# Add static files serving configuration
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(application)
